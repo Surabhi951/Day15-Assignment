346da33c634349a1ec41585285d44a6140211874
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-31 18:00:31 +0530

    find frequency of word in para

--- src/com/bridgelabz/HashMain.java
+++ src/com/bridgelabz/HashMain.java
@@ -3,25 +3,19 @@ package com.bridgelabz;
 public class HashMain {
 
     public static void main(String args[]) {
-        HashMap hashMap = new HashMap();
-        String sentence = "To be or not to be";
-        String[] words = sentence.toLowerCase().split(" ");
-        for (String word : words) {
-            hashMap.put(word, 1);
-        }
-        int frequency = hashMap.get("to");
-        System.out.println("Frequency of given word is " + frequency);
-        System.out.println();
 
-        HashMap hashMap1 = new HashMap();
-        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-        String[] words1 = paragraph.split(" ");
-        for ( String word : words1)
-        {
-            hashMap1.put(word,1);
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations" ;
+        String[] splitArray = paragraph.split(" ");
+        MyHashTable<String,Integer> hashTable = new MyHashTable<>();
+        for (String word:splitArray) {
+            Integer value = hashTable.get(word);
+            if(value ==  null){
+                value = 1;
+            }
+            else
+                value = value+1;
+            hashTable.add(word,value);
         }
-       int frequency1 = hashMap1.get("into");
-       System.out.println("Frequency of given word is "+frequency1);
-
+        hashTable.print();
     }
 }
