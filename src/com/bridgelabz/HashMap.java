9f5c383e6a66f1e32a517237dd708374a408587b
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-31 12:54:35 +0530

    find frequency of word in a large paragraph

--- src/com/bridgelabz/HashMap.java
+++ src/com/bridgelabz/HashMap.java
@@ -3,49 +3,40 @@ package com.bridgelabz;
 public class HashMap {
     HashLinkedList[] buckets;
 
-    public HashMap()
-    {
+    public HashMap() {
         buckets = new HashLinkedList[20];
     }
 
-    public void put(String key,Integer value)
-    {
+    public void put(String key, Integer value) {
         int bucketIndex = computeBucketIndex(key);
         HashLinkedList foundLinkedList = buckets[bucketIndex];
-        if(foundLinkedList != null)
-        {
+        if (foundLinkedList != null) {
             HashNode foundNode = foundLinkedList.search(key);
-            if(foundNode != null)
-            {
+            if (foundNode != null) {
                 foundNode.value += value;
-            }
-            else {
+            } else {
                 foundLinkedList.add(key, value);
             }
-        }
-        else
-        {
+        } else {
             HashLinkedList newLinkedList = new HashLinkedList();
-            newLinkedList.add(key,value);
+            newLinkedList.add(key, value);
             buckets[bucketIndex] = newLinkedList;
         }
     }
-    public int get(String key)
-    {
+
+    public int get(String key) {
         int bucketIndex = computeBucketIndex(key);
         HashLinkedList foundLinkedList = buckets[bucketIndex];
-        if(foundLinkedList != null)
-        {
+        if (foundLinkedList != null) {
             HashNode foundNode = foundLinkedList.search(key);
-            return  foundNode.value;
+            return foundNode.value;
         }
         return 0;
     }
 
-        private int computeBucketIndex(String key) {
+    public int computeBucketIndex(String key) {
         int hashCode = Math.abs(key.hashCode());
         int bucketIndex = hashCode % buckets.length;
         return bucketIndex;
     }
-
 }
